e2d890bd41408d0f4bf88c4a373ee8bedbb8665c
jakhongirs <j.sagdullaev31@gmail.com>
2021-05-13 18:38:17 +0500

    Site Header started

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Header.scss'
+import { Link, NavLink } from 'react-router-dom'
 import Container from '../Container/Container'
+
+/* Svg icons */
+import LogoImg from '../Lib/Svg/Logo'
 
 function Header() {
 	return (
 		<header className='site-header'>
 			<Container>
-				<div className='site-header__container'></div>
+				<div className='site-header__container'>
+					<Link className='logo' to='/'>
+						<LogoImg className='logo__img' />
+					</Link>
+				</div>
 			</Container>
 		</header>
 	)
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -5,7 +5,6 @@ function Home() {
 	return (
 		<>
 			<Hero />
-			JS
 		</>
 	)
 }
