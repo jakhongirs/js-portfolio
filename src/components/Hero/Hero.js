2c1941b73b9814933d37cea63af03333c2fd0397
jakhongirs <j.sagdullaev31@gmail.com>
2021-05-18 01:30:12 +0500

    FInished desktop version of hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,10 +2,62 @@ import React from 'react'
 import './Hero.scss'
 import Container from '../Container/Container'
 
+//Images and icons:
+import heroImg from '../../assets/images/hero-img.png'
+import Github from '../Lib/Svg/Github'
+import Twitter from '../Lib/Svg/Twitter'
+import Linkedin from '../Lib/Svg/Linkedin'
+
 function Hero() {
 	return (
 		<section className='hero'>
-			<Container></Container>
+			<Container>
+				<div className='hero__wrapper'>
+					<img className='hero__img' src={heroImg} alt='Jakhongir Sagdullaev' />
+
+					<div className='hero__texts-wrapper'>
+						<h1 className='hero__heading'>Jakhongir Sagdullaev</h1>
+						<p className='hero__job-position'>Software Developer @ Abutech</p>
+						<p className='hero__desc'>
+							I prefer React for the frontend, Node.js for backend development.
+							I love everything about JavaScript!
+						</p>
+
+						<ul className='hero__skills-list'>
+							<li className='hero__skills-item'>JavaScript</li>
+							<li className='hero__skills-item'>React</li>
+							<li className='hero__skills-item'>Next.js</li>
+							<li className='hero__skills-item'>TypeScript</li>
+							<li className='hero__skills-item'>Nodejs</li>
+							<li className='hero__skills-item'>Express</li>
+							<li className='hero__skills-item'>PostgreSQL</li>
+						</ul>
+
+						<ul className='hero__social-list'>
+							<li className='hero__social-item'>
+								<a className='hero__social-link' href='' target='blank'>
+									<Linkedin className='hero__social-img' />
+								</a>
+							</li>
+
+							<li className='hero__social-item'>
+								<a
+									className='hero__social-link'
+									href='https://github.com/jakhongirs'
+									target='blank'>
+									<Github className='hero__social-img' />
+								</a>
+							</li>
+
+							<li className='hero__social-item'>
+								<a className='hero__social-link' href='' target='blank'>
+									<Twitter className='hero__social-img' />
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</Container>
 		</section>
 	)
 }
